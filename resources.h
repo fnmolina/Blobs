e18c8d4fe8e9ca3a35c33f9aa6c227a206c0699c
fnmolina <famolina@itba.edu.ar>
2021-03-25 00:47:55 -0300

    Chau resources.h

--- resources.h
+++ resources.h
@@ -1,10 +1,6 @@
+
 #ifndef __RESOURCES
 #define __RESOURCES
-
-
-#define SCREEN_W	100
-#define SCREEN_H	100
-
 
 //image resources
 #define BACKGROUND "background.jpg"
@@ -13,34 +9,15 @@
 #define BLOB_SPRT "grownblob.png"
 #define OLD_SPRT "goodoldblob.png"
 
-
+#define FOOD_BITMAP_SIZE 20
+#define BABY_BITMAP_SIZE 40
+#define BLOB_BITMAP_SIZE 45
+#define OLD_BITMAP_SIZE 80
 
 //font
-#define FONT "Font/stupid.ttf"
+#define FONT "Font/Font.ttf"
 #define FONT_SIZE 30
-#define FONT_COLOR "white"
-
-
-struct userData {
-	int mode;
-	int initialBlobs;
-	int foodCount;
-	int speedMax;
-	float speedProb;
-	float smellRadius;
-	float randomJiggleLimit;
-	float deathProbBabyBlob;
-	float deathProbGrownBlob;
-	float deathProbOldBlob;
-};
-
-
-float random(int max);
-
-
-
-
+#define FONT_COLOR "yellow"
 
 
 #endif // __RESOURCES
-
